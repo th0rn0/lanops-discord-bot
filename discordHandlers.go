0f99d9f7577511a8ec912cd2bcd8692680ed6343
Th0rn0 <dev@th0rn0.co.uk>
2024-03-17 21:08:07 +0000

    message formatting

--- discordHandlers.go
+++ discordHandlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"lanops/discord-bot/api"
+	"strconv"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -22,23 +24,51 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		logger.Info().Msg("Message Create Event - Get Event - Triggered")
 		// DEBUG - Error Handling
 		var nextEvent = lanopsAPI.GetNextEvent()
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("body: %s", nextEvent))
+		s.ChannelMessageSend(m.ChannelID, formatNextEventMessage(nextEvent))
 	}
 
 	if m.Content == commandPrefix+"get dates" {
 		logger.Info().Msg("Message Create Event - Get Dates - Triggered")
 		var upcomingEvents = lanopsAPI.GetUpcomingEvents()
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("body: %s", upcomingEvents))
+		s.ChannelMessageSend(m.ChannelID, formatUpcomingEventDatesMessage(upcomingEvents))
 	}
 
 	if m.Content == commandPrefix+"get attendees" {
 		logger.Info().Msg("Message Create Event - Get Attendees - Triggered")
 		var participants = lanopsAPI.GetNextEventParticipants()
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("body: %s", participants))
+		s.ChannelMessageSend(m.ChannelID, formatEventParticipantsMessage(participants))
 	}
 }
 
 func ready(s *discordgo.Session, event *discordgo.Ready) {
 	// Set the playing status.
 	s.UpdateGameStatus(0, "BOT GO BRRR")
+}
+
+func formatEventParticipantsMessage(participants []api.EventParticipant) string {
+	var returnMessage = "\nAttendees for Next Event:\n"
+	for _, participant := range participants {
+		returnMessage += fmt.Sprintf(
+			"%s - %s\n", participant.Username, participant.Seat)
+	}
+	return returnMessage
+}
+
+func formatUpcomingEventDatesMessage(events []api.Event) string {
+	var returnMessage = "\nUpcoming Events:\n"
+	for _, event := range events {
+		returnMessage += fmt.Sprintf(
+			"%s - %s to %s\n", event.Name, event.Start, event.End)
+	}
+	return returnMessage
+}
+
+func formatNextEventMessage(event api.Event) string {
+	var returnMessage = "\nNext Event:\n"
+	returnMessage += event.Name + "\n"
+	returnMessage += event.Start + " to " + event.End + "\n"
+	returnMessage += event.Description.Short + "\n"
+	returnMessage += fmt.Sprintf("Seats: %s \n", strconv.Itoa(event.Capacity))
+	returnMessage += "BOOK NOW!: " + event.URL.Base + event.URL.Tickets
+	return returnMessage
 }
